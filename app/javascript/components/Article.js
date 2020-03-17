8b9616d875362de6e6fe7d0f068e0145e2d20f82
Adrián <adrianramaaguilar@gmail.com>
2020-03-16 22:21:59 -0400

    Complete changing article to React component on index, and added react datetime and moment respectively

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -1,14 +1,29 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+import Moment from 'react-moment';
+
 class Article extends React.Component {
+  
   render () {
+    var created_at = (new Date(this.props.created_at)).getTime();
+    var updated_at = (new Date(this.props.updated_at)).getTime();
     return (
       <React.Fragment>
-        <div class="article-title">
+        <div className="article-title">
           <a href={this.props.path}>{this.props.title}</a>
         </div>
-        <div class="article-body">
+        <div className="article-body">
           {this.props.description}
+          <div className="article-meta-details">
+            <small> 
+              Created by: {this.props.author},
+              &nbsp;
+              <Moment fromNow>{created_at}</Moment>,
+              &nbsp;
+              last updated: <Moment fromNow>{updated_at}</Moment>
+            </small>  
+          </div>
         </div>
       </React.Fragment>
     );
@@ -18,6 +33,9 @@ class Article extends React.Component {
 Article.propTypes = {
   title: PropTypes.string,
   path: PropTypes.string,
-  description: PropTypes.string
+  description: PropTypes.string,
+  author: PropTypes.string,
+  created_at: PropTypes.string,
+  updated_at: PropTypes.string
 };
 export default Article
